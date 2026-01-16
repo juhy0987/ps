2c52652b80b6c7add147d7a984b7f761717a0367
juhy0987 <juhy0987@naver.com>
2026-01-16 22:54:39 +0900

    [26.01.16] boj 12873 solved

---
+++ bojkr/02.silver/12873.py
@@ -0,0 +1,17 @@
+# 12873 기념품
+# https://www.acmicpc.net/problem/12873
+# Silver 3
+
+def main():
+    N = int(input())
+    L = [i+1 for i in range(N)]
+    cur = 0
+    for i in range(1, N):
+        index = (cur + i ** 3 - 1) % len(L)
+        L.pop(index)
+        cur = index % len(L)
+        # print(index, cur, L)
+    print(L[0])
+
+if __name__ == "__main__":
+    main()
